d7e6a685a01ba1adfabd09ab1a447cfe29be9649
Eric Grant <senatorericgrant@gmail.com>
2020-06-22 03:20:45 +0700

    feat: add the Film list screen

---
+++ MyMDBDemo/Views/FilmListScreenView.swift
@@ -0,0 +1,21 @@
+//
+//  FilmListScreenView.swift
+//  MyMDBDemo
+//
+//  Created by Eric Grant on 22.06.2020.
+//  Copyright © 2020 Eric Grant. All rights reserved.
+//
+
+import SwiftUI
+
+struct FilmListScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FilmListScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        FilmListScreenView()
+    }
+}
