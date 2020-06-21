388ca204947ea488ee95d68b0f623b5880bc89d6
Eric Grant <senatorericgrant@gmail.com>
2020-06-22 02:03:59 +0700

    feat: add Film poster carousel

--- MyMDBDemo/Views/FilmPosterCarouselView.swift
+++ MyMDBDemo/Views/FilmPosterCarouselView.swift
@@ -1,5 +1,5 @@
 //
-//  PosterCarouselView.swift
+//  FilmPosterCarouselView.swift
 //  MyMDBDemo
 //
 //  Created by Eric Grant on 22.06.2020.
@@ -8,13 +8,13 @@
 
 import SwiftUI
 
-struct PosterCarouselView: View {
+struct FilmPosterCarouselView: View {
     
     let filmTitle: String
     let films: [Film]
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 0) {
+        VStack(alignment: .leading, spacing: 14) {
             Text(filmTitle)
                 .font(.title)
                 .fontWeight(.bold)
@@ -23,19 +23,19 @@ struct PosterCarouselView: View {
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(alignment: .top, spacing: 14) {
                     ForEach(self.films) { film in
-                        PosterBackgroundView(film: film)
-                            .frame(width: 274, height: 210)
-                            .padding(.leading, film.id == self.films.first!.id ? 14 : 0)
-                            .padding(.trailing, film.id == self.films.last!.id ? 14 : 0)
+                        FilmPosterView(film: film)
+                        .padding(.leading, film.id == self.films.first!.id ? 14 : 0)
+                        .padding(.trailing, film.id == self.films.last!.id ? 14 : 0)
                     }
                 }
             }
         }
+        
     }
 }
 
-struct PosterCarouselView_Previews: PreviewProvider {
+struct FilmPosterCarouselView_Previews: PreviewProvider {
     static var previews: some View {
-        PosterCarouselView(filmTitle: "Latest", films: Film.getFilms)
+        FilmPosterCarouselView(filmTitle: "Now Playing", films: Film.getFilms)
     }
 }
