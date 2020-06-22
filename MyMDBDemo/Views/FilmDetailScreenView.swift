77ca4f56e40811c222c83af761e4c1d78e8b05be
Eric Grant <senatorericgrant@gmail.com>
2020-06-22 07:56:36 +0700

    feat: add the film poster to a detail screen

--- MyMDBDemo/Views/FilmDetailScreenView.swift
+++ MyMDBDemo/Views/FilmDetailScreenView.swift
@@ -11,14 +11,61 @@ import SwiftUI
 struct FilmDetailScreenView: View {
     
     let filmID: Int
+    @ObservedObject private var filmDetailCondition = FilmDetailCondition()
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            ScreenLoadingView(download: self.filmDetailCondition.downloading, error: self.filmDetailCondition.error) {
+                self.filmDetailCondition.loadFilm(id: self.filmID)
+            }
+            
+            if filmDetailCondition.film != nil {
+                FilmDetailList(film: self.filmDetailCondition.film!)
+            }
+        }
+        .navigationBarTitle(filmDetailCondition.film?.title ?? "")
+        .onAppear {
+            self.filmDetailCondition.loadFilm(id: self.filmID)
+        }
+    }
+}
+
+struct FilmDetailList: View {
+    
+    let film: Film
+    
+    var body: some View {
+        List {
+            FilmDetailPoster(posterURL: self.film.backgroundURL)
+            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+        }
+    }
+}
+
+struct FilmDetailPoster: View {
+    
+    let posterURL: URL
+    @ObservedObject private var posterLoader = PosterLoader()
+    
+    var body: some View {
+        ZStack {
+            Rectangle().fill(Color.gray.opacity(0.2))
+            if self.posterLoader.poster != nil {
+                Image(uiImage: self.posterLoader.poster!)
+                .resizable()
+            }
+        }
+        .aspectRatio(16/9, contentMode: .fill)
+        .onAppear {
+            self.posterLoader.loadPoster(with: self.posterURL)
+        }
     }
 }
 
 struct FilmDetailScreenView_Previews: PreviewProvider {
     static var previews: some View {
-        FilmDetailScreenView(filmID: Film.getFilm.id)
+        NavigationView {
+            FilmDetailScreenView(filmID: Film.getFilm.id)
+        }
     }
 }
